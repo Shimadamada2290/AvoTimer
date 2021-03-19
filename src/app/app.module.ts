c506f464f8fe3bd24f185b9e5ebd62c3bb5cfa7d
equalcoding <info@equalcoding.dev>
2021-03-19 22:09:23 +0000

    avotimer: first routing and screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './routes/app-routing.module';
+import { CategoryScreenComponent } from './components/category-screen/category-screen.component';
+import { RecipesScreenComponent } from './components/recipes-screen/recipes-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryScreenComponent,
+    RecipesScreenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
